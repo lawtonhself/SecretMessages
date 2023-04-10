853a59ac81a6aab1a24fc1ab024711923d839ce5
lawtonhself <lawtonhself@hotmail.com>
2023-04-10 16:12:50 -0400

    Finished
    -added rules for A-Z/a-z/0-9
    -added a method that will be recalled so you can keep encrypting or decrypting until you close the application

--- src/secretMessages/SecretMessages.java
+++ src/secretMessages/SecretMessages.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class SecretMessages {
 
 	public static void main(String[] args) {
+		codeIt();
+	}
+	public static void codeIt () {
 		//declaring a new scanner
 		Scanner scan = new Scanner(System.in);
 		//output to user to ask for message to encode or decode
@@ -13,15 +16,40 @@ public class SecretMessages {
 		String message = scan.nextLine();
 		//this will be the String that we will output to the user after encoding or decoding
 		String output = "";
-		//set the key that will be used for encoder/decoder
-		//we will just add 13 to each character of the message for encoding and subtract for decoding
-		char key = 13;
+		//prompts user to enter a number from -25 to 25 to use as the key for how the letters will shift like A = B(key = 1)
+		System.out.println("Enter a secret key (-25 to 25):");
+		int keyVal = Integer.parseInt(scan.nextLine());
+		char key = (char) keyVal;
 		
 		//For right now this for loop goes through and add the key(13) to each char and converts it to that char
-		//example A will equal N by adding 13
-		for (int x = 0; x < message.length(); x++) {
-			output += (char)(message.charAt(x) + key);
-		}
-		System.out.println(output);
+				//example A will equal N by adding 13
+				for ( int x = 0; x < message.length(); x++ ) {
+					char input = message.charAt(x);
+					if (input >= 'A' && input <= 'Z'){
+						input += key;
+						if (input > 'Z')
+							input -= 26;
+						if (input < 'A')
+							input += 26;
+					}
+					else if (input >= 'a' && input <= 'z'){
+						input += key;
+						if (input > 'z')
+							input -= 26;
+						if (input < 'a')
+							input += 26;
+					}
+					else if (input >= '0' && input <= '9') {
+						input += (keyVal % 10);
+						if (input > '9')
+							input -= 10;
+						if (input <'0')
+							input += 10;
+					}
+					output += input;
+				}
+				System.out.println(output);
+				//scan.close();
+				codeIt();
 	}
 }
